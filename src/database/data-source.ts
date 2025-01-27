b53b5ae2e8baced123107e94ff5b7ee8798edc99
Henrique Douglas <henriquedouglas2803@gmail.com>
2025-01-27 17:09:54 -0300

    atualiza template

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,18 +1,21 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import * as dotenv from "dotenv"
 
 import Livro from "../entities/Livro"
 import Auditorio from "../entities/Auditorio"
 import Leitor from "../entities/Leitor"
 import Autor from "../entities/Autor"
 
+dotenv.config()
+
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "admin",
-    database: "biblioteca",
+    type: process.env.DB_TYPE as any,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "5432"),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: true,
     entities: [Livro, Auditorio, Leitor, Autor],
--- src/routes/livro.routes.ts
+++ src/routes/livro.routes.ts
@@ -3,5 +3,4 @@ import { Request, Response, Router } from 'express';
 const livroRoutes = Router();
 
 
-
 export default livroRoutes;
